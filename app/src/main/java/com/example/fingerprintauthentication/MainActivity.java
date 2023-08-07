b9de2f835ffa84acb3e1d863f6109f38c0eba7f3
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-07 15:11:09 +0600

    day 2

--- app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
+++ app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.fingerprintauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView msg_text=findViewById(R.id.textMsg);
+        Button login_Btn=findViewById(R.id.loginBtn);
+
+        BiometricManager biometricManager= BiometricManager.from(this);
+        if(biometricManager.canAuthenticate()==BiometricManager.BIOMETRIC_SUCCESS){
+            msg_text.setText("You can use the fingerprint sensor to login");
+            msg_text.setTextColor(Color.rgb(255,255,255));
+        } else if (biometricManager.canAuthenticate()==BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE) {
+            msg_text.setText("the device don't have a fingerprint sensor");
+            login_Btn.setVisibility(View.GONE);
+        }else if (biometricManager.canAuthenticate()==BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE) {
+            msg_text.setText("the biometric sensors is currently unavailable");
+            login_Btn.setVisibility(View.GONE);
+        }else if (biometricManager.canAuthenticate()==BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED) {
+            msg_text.setText("your device don't have any fingerprint saved, Please check your security settings");
+            login_Btn.setVisibility(View.GONE);
+
+        }
     }
 }
